4a1951717cb18c61d41cbf56a487a818801fcfce
ShirleyAngie <nlshirley08@gmail.com>
2022-12-01 17:43:07 -0500

    commit 17h41

---
+++ app/src/main/java/uta/fisei/shirleynunez_prueba1/SecontActivity.java
@@ -0,0 +1,14 @@
+package uta.fisei.shirleynunez_prueba1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secont);
+    }
+}
