eef3390a059f2b1a1c5412b2e4df30186f14346e
ShirleyAngie <nlshirley08@gmail.com>
2022-12-01 19:35:23 -0500

    commit 19h35

--- app/src/main/java/uta/fisei/shirleynunez_prueba1/ThreeActivity.java
+++ app/src/main/java/uta/fisei/shirleynunez_prueba1/ThreeActivity.java
@@ -1,45 +1,57 @@
 package uta.fisei.shirleynunez_prueba1;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ThreeActivity extends AppCompatActivity {
 
-    EditText  editTextNombrestres,editTextApellidostres,editTextDividendotres, editTextDivisortres, editTextNumerotres;
+    EditText editTextNombres, editTextDividendo , editTextApellidos, editTextDivisor , editTextNumeros;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
-        //editTextNombrestres = editTextNombrestres.findViewById(R.id.editTextNombrestres);
-       //editTextApellidostres= editTextApellidostres.findViewById(R.id.editTextApellidostres);
-        editTextDividendotres= editTextDividendotres.findViewById(R.id.editTextDividendotres);
-        editTextDivisortres= editTextDivisortres.findViewById(R.id.editTextDivisortres);
-        editTextNumerotres= editTextNumerotres.findViewById(R.id.editTextNumerotres);
-
-
+        editTextNombres = findViewById(R.id.editTextNombrestres);
+        editTextDividendo = findViewById(R.id.editTextDividendotres);
+        editTextApellidos = findViewById(R.id.editTextApellidostres);
+        editTextDivisor = findViewById(R.id.editTextDivisortres);
+        editTextNumeros =  findViewById(R.id.editTextNumerotres);
     }
-     public void cerrar(View view){
-        //String nombres= editTextNombrestres.getText().toString();
-       //String apellidos = editTextApellidostres.getText().toString();
-        String dividendo = editTextDividendotres.getText().toString();
-        String divisor= editTextDivisortres.getText().toString();
-        String numero= editTextDivisortres.getText().toString();
+
+    public void cerrar (View view){
+        String apellidos = editTextApellidos.getText().toString();
+        String divisor = editTextDivisor.getText().toString();
+        String numero = editTextNumeros.getText().toString();
+        String nombres = editTextNombres.getText().toString();
+        String dividendo = editTextDividendo.getText().toString();
+
+        String[] lstDatos = new String[5];
+        lstDatos[0]= nombres;
+        lstDatos[1]=apellidos;
+        lstDatos[2]= dividendo;
+        lstDatos[3]=divisor;
+        lstDatos[4]=numero;
 
         Intent intent = new Intent();
-        //intent.putExtra("Nombres", nombres);
-        //intent.putExtra("Apellidos", apellidos);
-         intent.putExtra("Dividendo", dividendo);
-         intent.putExtra("Divisor", divisor);
-         intent.putExtra("Numero", numero);
-         setResult(Activity.RESULT_OK);
+        intent.putExtra("lstDatos",lstDatos);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
 
-         finish();
-     }
+
+
+
 
 }
